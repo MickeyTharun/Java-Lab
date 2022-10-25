26f394f3f23b3d957f7fd4d30a5b31760e80238d
Tharun M S <116061040+MickeyTharun@users.noreply.github.com>
2022-10-25 13:07:36 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,15 @@
+package firstproject;
+import java.io.*;
+
+public class Bitwise {
+		  public static void main(String[] args) throws IOException {
+		    int a = 20,    b = 15;
+		   
+		System.out.println("The result after performing num1&num2 " + (a & b));
+		    System.out.println("The result after performing num1|num2 " + (a | b));
+		System.out.println("The result after performing num1^num2 " + (a ^ b));
+		    System.out.println("The result after performing ~num2 " + (~a));
+		  }
+
+
+	}
